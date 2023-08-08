4968a2a8d598ddf89e922e9936c737b84af32e08
Your Name <feichen0826@gmail.com>
2023-08-08 15:44:37 -0700

    first draft of models

---
+++ backend/db/migrations/20230808223202-create-attendance.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Attendances', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      eventId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model:'Events'
+        }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model:'Users'
+        }
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values:['attending','waitlist','pending']
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Attendances');
+  }
+};
---
+++ backend/db/migrations/20230808223639-create-event.js
@@ -0,0 +1,63 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      venueId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model:'Venues'
+        }
+      },
+      groupId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model:'Groups'
+        }
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      type: {
+        type: Sequelize.ENUM,
+        values: ['Online', 'In person']
+      },
+      capacity: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      endDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+  }
+};
